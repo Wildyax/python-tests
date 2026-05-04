91f0f565a7039c5ca08e2a01b0d61dd78b17c632
Alex <alexom003@hotmail.fr>
2026-05-04 15:09:50 +0200

    check success coverage tests

--- test_app.py
+++ test_app.py
@@ -16,34 +16,34 @@ class ItemsApiTestCase(unittest.TestCase):
         response = self.client.get('/items')
         self.assertEqual(response.status_code, 200)
 
-    # def test_get_items_returns_list(self):
-    #     """GET /items should return a JSON list of items."""
-    #     response = self.client.get('/items')
-    #     data = response.get_json()  # parse JSON response into Python data
-    #     # Verify that the response is a list
-    #     self.assertIsInstance(data, list)
-    #     # There should be at least one item in the list (we added 2 by default)
-    #     self.assertGreaterEqual(len(data), 1)
+    def test_get_items_returns_list(self):
+        """GET /items should return a JSON list of items."""
+        response = self.client.get('/items')
+        data = response.get_json()  # parse JSON response into Python data
+        # Verify that the response is a list
+        self.assertIsInstance(data, list)
+        # There should be at least one item in the list (we added 2 by default)
+        self.assertGreaterEqual(len(data), 1)
 
-    # def test_items_have_id_and_name(self):
-    #     """Each item in the list should have 'id' and 'name' fields."""
-    #     response = self.client.get('/items')
-    #     data = response.get_json()
-    #     if len(data) > 0:
-    #         item = data[0]
-    #         # Check that item is a dict with the required keys
-    #         self.assertIsInstance(item, dict)
-    #         self.assertIn('id', item)
-    #         self.assertIn('name', item)
+    def test_items_have_id_and_name(self):
+        """Each item in the list should have 'id' and 'name' fields."""
+        response = self.client.get('/items')
+        data = response.get_json()
+        if len(data) > 0:
+            item = data[0]
+            # Check that item is a dict with the required keys
+            self.assertIsInstance(item, dict)
+            self.assertIn('id', item)
+            self.assertIn('name', item)
 
-    # def test_sample_item_present(self):
-    #     """The default 'Sample Item 1' should be part of the response."""
-    #     response = self.client.get('/items')
-    #     data = response.get_json()
-    #     # Collect all item names from the response
-    #     names = [item['name'] for item in data]
-    #     # Check that "Sample Item 1" is in the list of names
-    #     self.assertIn('Sample Item 1', names)
+    def test_sample_item_present(self):
+        """The default 'Sample Item 1' should be part of the response."""
+        response = self.client.get('/items')
+        data = response.get_json()
+        # Collect all item names from the response
+        names = [item['name'] for item in data]
+        # Check that "Sample Item 1" is in the list of names
+        self.assertIn('Sample Item 1', names)
 
     # def test_fail(self):
     #     """Just a fail test for pipeline github"""
--- test_integration.py
+++ test_integration.py
@@ -43,39 +43,39 @@ def test_insert_item(db_connection):
     assert result[0] == "Item A"
 
 
-# def test_multiple_items(db_connection):
-#     """Tester l'insertion de plusieurs items."""
-#     cursor = db_connection.cursor()
+def test_multiple_items(db_connection):
+    """Tester l'insertion de plusieurs items."""
+    cursor = db_connection.cursor()
 
-#     items = [("Item 1",), ("Item 2",), ("Item 3",)]
-#     cursor.executemany("INSERT INTO items (name) VALUES (?)", items)
-#     db_connection.commit()
+    items = [("Item 1",), ("Item 2",), ("Item 3",)]
+    cursor.executemany("INSERT INTO items (name) VALUES (?)", items)
+    db_connection.commit()
 
-#     cursor.execute("SELECT COUNT(*) FROM items")
-#     count = cursor.fetchone()[0]
+    cursor.execute("SELECT COUNT(*) FROM items")
+    count = cursor.fetchone()[0]
 
-#     assert count == 3
-
-
-# def test_item_structure(db_connection):
-#     """Tester que les colonnes retournées sont correctes."""
-#     cursor = db_connection.cursor()
-
-#     cursor.execute("INSERT INTO items (name) VALUES (?)", ("Test Item",))
-#     db_connection.commit()
-
-#     cursor.execute("SELECT id, name FROM items")
-#     row = cursor.fetchone()
-
-#     assert isinstance(row[0], int)  # id
-#     assert isinstance(row[1], str)  # name
+    assert count == 3
 
 
-# def test_empty_table(db_connection):
-#     """Tester qu'une table vide retourne aucun résultat."""
-#     cursor = db_connection.cursor()
+def test_item_structure(db_connection):
+    """Tester que les colonnes retournées sont correctes."""
+    cursor = db_connection.cursor()
 
-#     cursor.execute("SELECT * FROM items")
-#     rows = cursor.fetchall()
+    cursor.execute("INSERT INTO items (name) VALUES (?)", ("Test Item",))
+    db_connection.commit()
 
-#     assert rows == []
+    cursor.execute("SELECT id, name FROM items")
+    row = cursor.fetchone()
+
+    assert isinstance(row[0], int)  # id
+    assert isinstance(row[1], str)  # name
+
+
+def test_empty_table(db_connection):
+    """Tester qu'une table vide retourne aucun résultat."""
+    cursor = db_connection.cursor()
+
+    cursor.execute("SELECT * FROM items")
+    rows = cursor.fetchall()
+
+    assert rows == []
